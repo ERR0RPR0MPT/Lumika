9dbf984694d42dcd97b88c68f5851b5f41fdcaaf
ERR0RPR0MPT <github@fwder.cn>
2023-09-10 08:02:14 +0800

    Chore: 升级到 v3.7.2 版本

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 
 const (
 	lumikaVersionNum      = 3
-	lumikaVersionString   = "v3.7.1"
+	lumikaVersionString   = "v3.7.2"
 	en                    = "Encode:"
 	de                    = "Decode:"
 	add                   = "Add:"
