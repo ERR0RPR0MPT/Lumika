cdcdbaf1b8324affacc38103bbf38ecaecbd7829
ERR0RPR0MPT <github@fwder.cn>
2023-10-07 01:39:14 +0800

    Fix: 降低下载哔哩源视频的线程数，减少错误率

--- utils/variable.go
+++ utils/variable.go
@@ -37,8 +37,8 @@ const (
 	DefaultBlankStartByte           = 86
 	DefaultBlankEndByte             = 87
 	DefaultDeleteFecFiles           = true
-	DefaultBiliDownloadGoRoutines   = 20
-	DefaultBiliDownloadsMaxQueueNum = 5
+	DefaultBiliDownloadGoRoutines   = 5
+	DefaultBiliDownloadsMaxQueueNum = 3
 	DefaultTaskWorkerGoRoutines     = 5
 	DefaultBiliDownloadMaxRetries   = 100
 	DefaultBiliDownloadReferer      = "https://www.bilibili.com"
