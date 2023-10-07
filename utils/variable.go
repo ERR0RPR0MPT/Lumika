9c67624b8c7579a5fabfb905dc9c051f09d98e68
ERR0RPR0MPT <github@fwder.cn>
2023-10-08 03:38:12 +0800

    Feat: 更新到 v3.8.5 Feat: 新增 <meta name="theme-color" content="#ffffff"> 的适配

--- utils/variable.go
+++ utils/variable.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	LumikaVersionNum                = 3
-	LumikaVersionString             = "v3.8.4"
+	LumikaVersionString             = "v3.8.5"
 	LumikaWorkDirName               = "lumika_data"
 	LumikaConfigFileName            = "lumika_config"
 	InitStr                         = "Init:"
